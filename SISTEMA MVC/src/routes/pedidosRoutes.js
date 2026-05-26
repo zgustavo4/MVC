add0eb9749337775419373db364ebb108e518e7c
Gustavo Alves da Silva <gustavoalves20silva@gmail.com>
2026-05-26 15:19:50 -0300

    pedidos

--- SISTEMA MVC/src/routes/pedidosRoutes.js
+++ SISTEMA MVC/src/routes/pedidosRoutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const pedidosController = require('../controllers/pedidosController')
 
-router.get('/pedidos', pedidosController.index)
-router.delete('/pedidos/id', pedidosController.delete)
+router.get('/pedidos', pedidosController.listar)
+router.post('/pedidos', pedidosController.criar)
+router.put('/pedidos/:id', pedidosController.atualizar)
+router.delete('/pedidos/:id', pedidosController.deletar)
 module.exports = router
