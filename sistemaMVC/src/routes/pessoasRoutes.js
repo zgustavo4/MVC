c704284887f033d7badc0602d8158e129e7119be
Beatriz-Giacomini <beatrizgiacomini17@gmail.com>
2026-05-28 17:06:38 -0300

    Delete sistemaMVC directory

--- sistemaMVC/src/routes/pessoasRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const pessoaController = require('../controller/pessoasController')
-
-router.get('/pessoas', pessoaController.index)
-router.delete('/pessoas/:id', pessoaController.delete)
-
-module.exports = router
--- sistemaMVC/src/routes/produtosRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const produtosController = rquire('../controller/produtosController')
-
-router.get('/produtos', produtosController.index)
-
-module.exports = router
